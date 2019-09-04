964628e0e9bb8024726afe1ca70c03a8fc49d0e4
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-09-04 14:08:24 +0700

    Add winner state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 // component
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
+}
+
 function Square(props) {
     return (
         <button
@@ -25,6 +46,11 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice()
+
+        if (calculateWinner(squares) || squares[i]) {
+            return
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O'
         this.setState({
             squares: squares,
@@ -42,7 +68,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if (winner) {
+            status = 'Winner: ' + winner
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        }
+
 
         return (
             <div>
