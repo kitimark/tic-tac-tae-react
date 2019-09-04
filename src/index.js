d7755ce864c7fb2a967b361cf70fd727cb4c2661
Wasutan Kitijerapat <markdog_@hotmail.com>
2019-09-04 12:17:58 +0700

    Update state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import './index.css'
 
 // component
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => {
+                    this.setState({value: 'X'})
+            }}
+            >
+                {this.state.value}
             </button>
         )
     }
@@ -15,7 +27,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />
+        return <Square value={i} />
     }
 
     render() {
